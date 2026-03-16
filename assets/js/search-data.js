ff5e3d939a1aa9eccbd7556bd2866d45867a1548
reza92farahani <56598479+reza92farahani@users.noreply.github.com>
2026-03-16 16:04:53 +0000

    Deploying to gh-pages from @ reza92farahani/reza92farahani.github.io@cc3ec2a53b61163a18540ae37006f759cf94b1c3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -77,16 +77,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather.html";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2.html";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-our-paper-on-energy-aware",
+          title: 'Our paper on energy-aware….',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',

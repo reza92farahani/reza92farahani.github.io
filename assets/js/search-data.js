3d29c56314af3749ca7d609a6a45ca31b39f8195
reza92farahani <56598479+reza92farahani@users.noreply.github.com>
2026-03-17 14:24:02 +0000

    Deploying to gh-pages from @ reza92farahani/reza92farahani.github.io@e9c6cd76aa2a772145376a940a1082b0f3fe075c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -433,12 +433,10 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-news-3",
-          title: 'News_3',
+            },},{id: "news-read-my-interview-entiteld-mit-drohnen-und-intelligentem-edge-computing-zur-brückeninspektion-der-zukunft-link",
+          title: 'Read my interview entiteld Mit Drohnen und intelligentem Edge Computing zur Brückeninspektion der...',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/news_3/";
-            },},{id: "news-our-paper-on-lightweight-webassembly-based-intrusion-detection-for-zero-trust-edge-networks-has-been-accepted-ieee-access-2026-link",
+          section: "News",},{id: "news-our-paper-on-lightweight-webassembly-based-intrusion-detection-for-zero-trust-edge-networks-has-been-accepted-ieee-access-2026-link",
           title: 'Our paper on Lightweight WebAssembly-Based Intrusion Detection for Zero Trust Edge Networks has...',
           description: "",
           section: "News",},{id: "news-our-paper-on-ellmpeg-an-edge-based-agentic-llm-video-processing-tool-has-been-accepted-acm-mmsys-2026-link",
@@ -447,12 +445,10 @@ ninja.data = [{
           section: "News",},{id: "news-our-paper-on-performance-evaluation-of-privacy-models-for-data-streams-on-the-edge-has-been-accepted-internet-technology-letter-2026-link",
           title: 'Our paper on Performance Evaluation of Privacy Models for Data Streams on the...',
           description: "",
-          section: "News",},{id: "news-news-4",
-          title: 'News_4',
+          section: "News",},{id: "news-happy-to-share-that-our-eu-horizon-graph-massivizer-project-and-the-graph-choreographer-tool-have-been-successfully-evaluated",
+          title: 'Happy to share that our EU Horizon Graph-Massivizer project and the Graph-Choreographer tool...',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/news_4/";
-            },},{id: "news-happy-to-announce-that-i-have-been-awarded-a-6-month-eu-enfield-research-exchange-grants-from-eu-horizon-enfield-project-hosted-at-sintef-norway-focusing-on-edge-ai-and-green-ai-for-onboard-analytics",
+          section: "News",},{id: "news-happy-to-announce-that-i-have-been-awarded-a-6-month-eu-enfield-research-exchange-grants-from-eu-horizon-enfield-project-hosted-at-sintef-norway-focusing-on-edge-ai-and-green-ai-for-onboard-analytics",
           title: 'Happy to announce that I have been awarded a 6-month EU ENFIELD research...',
           description: "",
           section: "News",},{id: "projects-project-1",
